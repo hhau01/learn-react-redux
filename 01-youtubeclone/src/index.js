22dcec8d4a75092c999aa53c5124026d6ed7eff2
Henry Au <henryau01@gmail.com>
2017-03-08 21:54:41 -0800

    signup youtube-api-search

--- 01-youtubeclone/src/index.js
+++ 01-youtubeclone/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyAG_EjjXsIxH6SsGR9wuOu_Yfhl4XHjn6c';
+
 // create new component. this component should produce some html
 // ES6 syntax for function
 const App = () => {
