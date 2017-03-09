5b703fcf4a31bb1a805553026f3e71d3e522101e
Henry Au <henryau01@gmail.com>
2017-03-08 22:21:04 -0800

    intro to state

--- 01-youtubeclone/src/components/search_bar.js
+++ 01-youtubeclone/src/components/search_bar.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        // update state to contain search term
+        this.state = { term: '' };
+    }
+
     render() {
         // event onChange
         return <input onChange={ event => console.log(event.target.value) } />;
