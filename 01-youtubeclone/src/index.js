186682764e6d774225e3d2eefebebe418292bc6f
Henry Au <henryau01@gmail.com>
2017-03-08 21:41:52 -0800

    hi!

--- 01-youtubeclone/src/index.js
+++ 01-youtubeclone/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// create new component. this component should produce some html
+// ES6 syntax for function
+const App = () => {
+    return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// take this component's generated html and put it on the page (in the DOM)
+// <App /> == <App></App>
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
