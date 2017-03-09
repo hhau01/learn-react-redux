1d3283eb796f6e46acf257054e5441df8bc1069b
Henry Au <henryau01@gmail.com>
2017-03-08 22:45:47 -0800

    refactor functional components to class

--- 01-youtubeclone/src/index.js
+++ 01-youtubeclone/src/index.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAG_EjjXsIxH6SsGR9wuOu_Yfhl4XHjn6c';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data)
-});
-
 // create new component. this component should produce some html
-// ES6 syntax for function
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            // this.setState({ videos: videos });
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 // take this component's generated html and put it on the page (in the DOM)
