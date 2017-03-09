a83ef84135f51111c26fcaf6f43ec75276f0c44e
Henry Au <henryau01@gmail.com>
2017-03-09 01:17:37 -0800

    search for videos

--- 01-youtubeclone/src/components/search_bar.js
+++ 01-youtubeclone/src/components/search_bar.js
@@ -13,12 +13,16 @@ class SearchBar extends Component {
         return (
             <div className="search-bar">
                 <input 
-                    value = {this.state.term}
-                    onChange = { event => this.setState({ term: event.target.value })} />
+                    value={this.state.term}
+                    onChange={event => this.onInputChange(event.target.value)} />
             </div>
         );
     }
 
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
+    }
 }
 
 export default SearchBar;
