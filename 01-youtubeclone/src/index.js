33ded1b378931e96dd777e239d29ff5ef2bdeafb
Henry Au <henryau01@gmail.com>
2017-03-08 22:38:39 -0800

    youtube search api response

--- 01-youtubeclone/src/index.js
+++ 01-youtubeclone/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAG_EjjXsIxH6SsGR9wuOu_Yfhl4XHjn6c';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data)
+});
 
 // create new component. this component should produce some html
 // ES6 syntax for function
@@ -17,5 +22,4 @@ const App = () => {
 
 // take this component's generated html and put it on the page (in the DOM)
 // <App /> == <App></App>
-
 ReactDOM.render(<App />, document.querySelector('.container'));
