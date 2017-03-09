acdc8b2e45e34348a79b61b3b888e9b8130bcfdd
Henry Au <henryau01@gmail.com>
2017-03-08 22:02:47 -0800

    export component

--- 01-youtubeclone/src/index.js
+++ 01-youtubeclone/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyAG_EjjXsIxH6SsGR9wuOu_Yfhl4XHjn6c';
 
 // create new component. this component should produce some html
 // ES6 syntax for function
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // take this component's generated html and put it on the page (in the DOM)
