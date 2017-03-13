a37afbaea6b142ce273c0b46d370cf37e5ca5a8b
Henry Au <henryau01@gmail.com>
2017-03-13 13:49:31 -0700

    condtional rendering

---
+++ 2-Redux/src/reducers/reducer_active_book.js
@@ -0,0 +1,10 @@
+
+// state argument is not application state, on the state this reducer is responsible for
+// if state is undefined, set it to null
+export default function(state = null, action) {
+    switch(action.type) {
+        case 'BOOK_SELECTED':
+            return action.payload;
+    }
+    return state;
+}
--- 2-Redux/src/reducers/reducer_books.js
+++ 2-Redux/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: 'The Notebook' },
-        { title: 'Harry Potter' },
-        { title: 'Pokemon'},
-        { title: 'Clean Code' }
+        { title: 'The Notebook', pages: 101},
+        { title: 'Harry Potter', pages: 987},
+        { title: 'Pokemon', pages: 85},
+        { title: 'Clean Code', pages: 1021}
     ]
 }
