2e32240071716bd526e26351847d4ddd4cc3c9a3
Henry Au <henryau01@gmail.com>
2017-03-13 18:29:05 -0700

    avoid state mutations in reducers

--- 3-WeatherApp/src/reducers/reducer_weather.js
+++ 3-WeatherApp/src/reducers/reducer_weather.js
@@ -1,5 +1,13 @@
-export default function(state = null, action) {
-    console.log('Action received', action);
-    
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+    switch(action.type) {
+        case FETCH_WEATHER:
+            // don't try to manipulate state, instead do this
+            // return state.concat([action.payload.data]);
+            // insert to front of array
+            return [ action.payload.data, ...state ]; // [city, city, city]
+    }
+
     return state;
 }
