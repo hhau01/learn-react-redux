15e0d300598c78e9c6f1c8ac28cafe0fa959bcc3
Henry Au <henryau01@gmail.com>
2017-03-13 18:01:14 -0700

    more redux-promise

--- 3-WeatherApp/src/actions/index.js
+++ 3-WeatherApp/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '2934389bfbddfe64956847242e4e1899';
-const ROOT_URL = `http://api.openweatherapp.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 // so FETCH_WEATHER stays consistent
 export const FETCH_WEATHER = 'FETCH_WEATHER';
@@ -10,6 +10,8 @@ export function fetchWeather(city) {
     // only US country code for now
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
+
+    console.log('Request:', request);
 
     return {
         type: FETCH_WEATHER,
