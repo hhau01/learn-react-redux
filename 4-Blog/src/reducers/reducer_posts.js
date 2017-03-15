027ee8427e1f889937798857ce35719e57e2c728
Henry Au <henryau01@gmail.com>
2017-03-14 19:17:46 -0700

    catch data with posts reducer

---
+++ 4-Blog/src/reducers/reducer_posts.js
@@ -0,0 +1,14 @@
+// add reducer to reducers/index
+import { FETCH_POSTS } from '../actions/index';
+
+// all - empty list of blog posts
+const INITIAL_STATE = { all: [], post: null };
+
+export default function(state = INITIAL_STATE, action) {
+    switch(action.type) {
+        case FETCH_POSTS:
+
+        default:
+            return state;
+    }
+} 
