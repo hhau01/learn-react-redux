baef4b7cfa129c5b9561df88a8d2c3489ee6c2aa
Henry Au <henryau01@gmail.com>
2017-03-29 13:13:35 -0700

    loading data on render

--- 4-Blog/src/reducers/reducer_posts.js
+++ 4-Blog/src/reducers/reducer_posts.js
@@ -1,11 +1,13 @@
 // add reducer to reducers/index
-import { FETCH_POSTS } from '../actions/index';
+import { FETCH_POSTS, FETCH_POST } from '../actions/index';
 
 // all - empty list of blog posts
 const INITIAL_STATE = { all: [], post: null };
 
 export default function(state = INITIAL_STATE, action) {
     switch(action.type) {
+        case FETCH_POST:
+            return {...state, post: action.payload.data };
         case FETCH_POSTS:
             return { ...state, all: action.payload.data };
         default:
