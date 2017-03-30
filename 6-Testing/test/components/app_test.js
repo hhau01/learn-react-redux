3e4532236dc021c7f7307f4f8a34e03fe61c5c1b
Henry Au <henryau01@gmail.com>
2017-03-29 17:54:59 -0700

    write first test

--- 6-Testing/test/components/app_test.js
+++ 6-Testing/test/components/app_test.js
@@ -2,10 +2,20 @@ import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
 // Use 'describe' to group similar tests
-describe('App')
+describe('App', () => {
 
-// Use 'it' to test a single attribute of a target
-it('shows the correct text')
+  // Use 'it' to test a single attribute of a target
+  it('shows the correct text', () => {
 
-// Use 'expect' to make an 'assertion' about a target
-expect
+    // Create an instance of App
+    const component = renderComponent(App);
+
+    // Use 'expect' to make an 'assertion' about a target
+    // expect is a function that returns an object
+    // E.g. expect(component).to.have.class('comment-box')
+    expect(component).to.contain('Hello!');
+
+  });
+
+});
+
